122d59663429ccc54968f350da7c9c05ef0a4201
rogerdpack <rogerpack2005@gmail.com>
2010-05-21 16:19:15 -0600

    docs

--- spec/spec.split_parameters.rb
+++ spec/spec.split_parameters.rb
@@ -9,7 +9,56 @@ describe SplitParameters do
   it "should still allow for the original"
   it "should throw if you call a poor method name"
   it "should throw if you call an out of order method name"
-  
+
+=begin
+
+
+
+move_from_x(3).to_y(4)
+
+move_from(3).to(4)
+
+move_from(3,4)
+
+def move_from_x_to_y(x, to_y)
+end
+
+
+def move_from_x_with_sq_y(x, y)
+end
+
+
+move_from(3).with_sq(34)
+
+
+move_from_x(3).with_sq_y(34)
+
+move_from(:x =>3, :y => 34))
+
+
+
+setNumeratorandDenominator
+
+[frac2 setNumerator: 1 andDenominator: 5];
+
+
+-(void) setNumerator: (int) n andDenominator: (int) d {
+
+
+frac2. setNumerator: 1 andDenominator: 5
+
+
+def set(numerator, denominator)
+
+end
+
+set.numerator(n).and_denominator(d)
+
+set_numerator(n).and_denominator(d)
+
+
+=end
+
 end
     
     
